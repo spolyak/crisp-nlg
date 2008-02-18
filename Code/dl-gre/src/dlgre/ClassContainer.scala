19466127c744cbb31b0668c054367f4143076544
akoller <akoller@608a4d63-202c-0410-8d68-2b125dd3dbeb>
2008-02-18 17:36:20 +0000

    Cleanup.

--- Code/dl-gre/src/dlgre/ClassContainer.scala
+++ Code/dl-gre/src/dlgre/ClassContainer.scala
@@ -1,16 +1,17 @@
 package dlgre;
 
-import scala.collection.immutable.HashSet;
+import scala.collection.mutable.HashSet;
+import scala.collection.mutable.Set;
 
 import dlgre.formula._;
 
 class ClassContainer(graph:Graph) {
-	var classes : Set[Formula] = new HashSet[Formula]; // TODO hashcode for subset
+	val classes : Set[Formula] = new HashSet[Formula]; // TODO hashcode for subset
 	val simplifier = new Simplifier(graph);
         
         
         classes += new Top();
-        
+
         
         
         def getClasses = {
@@ -22,10 +23,7 @@ class ClassContainer(graph:Graph) {
         }
         
         def add(subset:Formula) = {
-          //println("add: " + subset.prettyprint);
-          
           if( classes.exists { cl => cl.extension(graph) == subset.extension(graph) } ) {
-            //println(" - already known");
             false
           } else {
             if( isNontrivial(subset) && isInformative(subset) ) {
@@ -36,32 +34,15 @@ class ClassContainer(graph:Graph) {
             	  val inter = new Conjunction(List(subset,other));
                     
             	  if( isNontrivial(inter) && isInformative(inter)  ) {
-            	    //println(" - really add: " + inter.prettyprint); 
-            	    classes = classes + inter;
-            	  } else {
-                  /*      
-                 if( !isNontrivial(inter) ) {
-                   println(" - (" + inter.prettyprint + " is empty)");
-                 }
-                 
-                 if( !isInformative(inter) ) {
-                   println(" - (" + inter.prettyprint + " is not informative)");
-                 }
-                 */
-                 }
-                } else {
-                  //println(" - (" + other.prettyprint + " is singleton)");
+            	    classes +=  inter;
+                  }
                 }
               }
             
               removeUninformativeSubsets();
             
-              //println(" - classes are now: " + classes.map { cl => cl.prettyprint} );
-            
               true
             } else {
-              //println(" - no ");
-            
               false
             }
           }
@@ -72,43 +53,37 @@ class ClassContainer(graph:Graph) {
           ! fmla.extension(graph).isEmpty
         }
         
-        // TODO - argh inefficient!
         private def isInformative(subset:Formula) = {
-          val individuals = subsetToSet(subset);
+          val individuals = subset.extension(graph);
+          val unionOverSubsets = new HashSet[String];
+          
+          classes.foreach { cl =>
+          	val ext = cl.extension(graph);
+                  
+                if( ext subsetOf individuals ) {
+                  unionOverSubsets ++= ext;
+                }
+          }
+          
+          individuals != unionOverSubsets
+          
+
+          /*
           val allSubsets = (for(s <- classes if subsetToSet(s) subsetOf individuals) yield subsetToSet(s)).toList;
           
          // println("   - informativity of " + subset);
           ! isCompletelyCovered(individuals.toList, allSubsets)
-        }
-        
-        private def isCompletelyCovered(individuals:List[String], allSubsets:List[Set[String]]) : Boolean = {
-          //println("    - icc " + individuals + " with " + allSubsets);
-          individuals match {
-            case Nil => true;
-            case head::tail => allSubsets.exists { sub =>
-            			if( sub.contains(head) ) {
-                                      isCompletelyCovered(tail.remove {x => sub.contains(x)}, allSubsets)
-                                } else {
-                                  false
-                                }
-                      }
-          }
-        }
-        
-        
-        private def subsetToSet(subset:Formula) : Set[String] = {
-          new HashSet[String] ++ subset.extension(graph)
+          */
         }
         
         private def removeUninformativeSubsets() = {
           val elements = classes.toList;
           
           elements.foreach { sub =>
-          	val backup = classes;
-                classes = classes - sub;
+                classes -= sub;
                 
                 if( isInformative(sub) ) {
-                  classes = backup;
+                  classes += sub;
                 }
           }
         }
