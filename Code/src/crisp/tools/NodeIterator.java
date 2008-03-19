67d3b6b3239892bc1e58365b88dc6bd6477be7ea
marktwilding <marktwilding@608a4d63-202c-0410-8d68-2b125dd3dbeb>
2008-03-19 17:55:53 +0000

    Utility for iterating over nodes

---
+++ Code/src/crisp/tools/NodeIterator.java
@@ -0,0 +1,74 @@
+package crisp.tools;
+
+import java.util.Iterator;
+
+import org.w3c.dom.Node;
+
+/**
+ * Iterates over the daughters of the given Node.
+ * Only returns nodes of element type if onlyElements is set to true.
+ * 
+ * @author Mark Wilding
+ */
+public class NodeIterator implements Iterator<Node> {
+	private Node parent;
+	private Node currentDaughter;
+	private boolean onlyElements = false;
+	
+	public NodeIterator(Node parent, boolean onlyElements) {
+		this.parent = parent;
+		this.onlyElements = onlyElements;
+		
+		if (parent==null) currentDaughter = null;
+		else currentDaughter = parent.getFirstChild();
+		
+		// If this is a node, but not an element, get the next element
+		if (onlyElements && currentDaughter!=null && currentDaughter.getNodeType()!=Node.ELEMENT_NODE) {
+			currentDaughter = getNextElement(currentDaughter);
+		}
+	}
+	
+	public NodeIterator(Node parent) {
+		this.parent = parent;
+		currentDaughter = parent.getFirstChild();
+	}
+	
+	private Node getNextElement(Node current) {
+		Node next = current;
+		
+		if (next==null) return null;
+		else next = next.getNextSibling();
+		
+		if (onlyElements) {
+			while (next!=null) {
+				if (next.getNodeType()==Node.ELEMENT_NODE) {
+					return next;
+				} else {
+					next = next.getNextSibling();
+				}
+			}
+			return null;
+		} else {
+			return next;
+		}
+	}
+
+	@Override
+	public boolean hasNext() {
+		return currentDaughter!=null;
+	}
+
+	@Override
+	public Node next() {
+		Node daughter = currentDaughter;
+		// Get the next sibling, or next element if onlyElements is set
+		currentDaughter = getNextElement(currentDaughter);
+		return daughter;
+	}
+
+	@Override
+	public void remove() {
+		// Can't remove things
+	}
+
+}
