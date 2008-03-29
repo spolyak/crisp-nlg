cfca5574a989acc3b6c85b2a67b7ab8f2bb01926
akoller <akoller@608a4d63-202c-0410-8d68-2b125dd3dbeb>
2008-03-29 19:55:52 +0000

    Added a decent hashCode to make it a bit faster.

--- Code/src/de/saar/chorus/term/Variable.java
+++ Code/src/de/saar/chorus/term/Variable.java
@@ -1,8 +1,8 @@
 /*
  * @(#)Variable.java created 11.05.2006
- * 
+ *
  * Copyright (c) 2006 Alexander Koller
- *  
+ *
  */
 
 package de.saar.chorus.term;
@@ -11,24 +11,29 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Variable extends Term {
-    private String name;
+    private final String name;
+    private final int hashcode;
 
     public Variable(String name) {
         this.name = name;
+        hashcode = name.hashCode();
     }
 
     public String getName() {
         return name;
     }
-    
 
+
+    @Override
     public String toString() {
         return name;
     }
 
+    @Override
     public Type getType() {
         return Type.VARIABLE;
     }
+    @Override
     public boolean equals(Object obj) {
         if( obj instanceof Variable ) {
             Variable vo = (Variable) obj;
@@ -38,10 +43,17 @@ public class Variable extends Term {
         }
     }
 
+    @Override
+    public int hashCode() {
+        return hashcode;
+    }
+
+    @Override
     public boolean hasSubterm(Term other) {
         return equals(other);
     }
 
+    @Override
     public Substitution getUnifier(Term other) {
         if( !other.hasSubterm(this)) {
             return new Substitution(this, other);
@@ -50,6 +62,7 @@ public class Variable extends Term {
         }
     }
 
+    @Override
     public Set<Variable> getVariables() {
         Set<Variable> ret = new HashSet<Variable>();
         ret.add(this);
@@ -60,7 +73,7 @@ public class Variable extends Term {
 	public String toLispString() {
 		return name;
 	}
-    
 
-    
+
+
 }
